fad496f3b27160e92ba9f1fba904e4cda4e24172
Yuriy Shandor <yuriyshandor@gmail.com>
2018-03-04 12:46:49 +0200

    Add Footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
+  // Header menu
   $('.menu-icon').on('click', function() {
     $('.header-nav').toggleClass('header-nav_show');
+  });
+
+  // Trasted slider
+  $(".trasted-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: $(".trasted-slider__nav_next"),
+    prevArrow: $(".trasted-slider__nav_prev"),
+    autoplay: true,
+   autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
