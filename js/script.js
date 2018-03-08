306099cdaed752071ce136ab20469203789114b5
Yuriy Shandor <yuriyshandor@gmail.com>
2018-03-08 10:37:40 +0200

    Add Rolling Stock. Mod_02

--- js/script.js
+++ js/script.js
@@ -44,4 +44,25 @@ $(document).ready(function() {
   $('.modal-form_close').click(function() {
     $('.modal-form').removeClass('modal-form_show');
   })
+
+  // Counter for Rolling Stock
+  var stockStatisticCounterStart = 0;
+  $(window).scroll(function() {
+    var oTop = $('.stock__statistic').offset().top - window.innerHeight;
+    if (stockStatisticCounterStart == 0 && $(window).scrollTop() > oTop) {
+      $('.stock__statistic-amount').each(function () {
+        $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+        },
+        {
+          duration: 3000,
+          easing: 'swing',
+          step: function (now) {
+          $(this).text(Math.ceil(now));
+        }
+        });
+      });
+      stockStatisticCounterStart = 1;
+    }
+  });
 });
